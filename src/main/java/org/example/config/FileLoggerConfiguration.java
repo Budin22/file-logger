e6c6b6db57f8a4518393f3ecaa9f5920354a9c48
Sergii Budin <apserbud22@gmail.com>
2023-11-01 14:27:09 +0200

    add FileLoggerConfiguration

---
+++ src/main/java/org/example/config/FileLoggerConfiguration.java
@@ -0,0 +1,8 @@
+package org.example.config;
+
+public class FileLoggerConfiguration {
+    private String pathToFile;
+    private LoginLevel level;
+    private int maxSize;
+    private String format;
+}
