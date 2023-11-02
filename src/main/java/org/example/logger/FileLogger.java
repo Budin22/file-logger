4afc9b38735de204f433324d818d4b4ff5770404
Sergii Budin <apserbud22@gmail.com>
2023-11-02 12:15:18 +0200

    add methods writeMessage, debug, info, and checkFileSizeLimitation to FileLogger

--- src/main/java/org/example/logger/FileLogger.java
+++ src/main/java/org/example/logger/FileLogger.java
@@ -1,2 +1,41 @@
-package org.example.logger;public class FileLogger {
+package org.example.logger;
+
+import org.example.config.LoginLevel;
+import org.example.exception.FileMaxSizeReachedException;
+
+import java.io.*;
+import java.util.Date;
+
+public class FileLogger {
+    private static String filePath = "src/main/resources/logs/text.txt";
+    private static long maxSize = 1;
+
+    private static void checkFileSizeLimitation() throws FileMaxSizeReachedException {
+        File myFile = new File(filePath);
+        long fileSize = myFile.length();
+        if(fileSize >= maxSize){
+            throw new FileMaxSizeReachedException(String.format("File size: %d bytes, but maxSize: %d bytes, file: %s", fileSize, maxSize, filePath));
+        }
+    }
+
+    private static void writeMessage(String message, String level){
+        try(FileWriter file = new FileWriter(filePath, true);
+            BufferedWriter writer = new BufferedWriter(file)
+        ) {
+            checkFileSizeLimitation();
+            writer.append(String.format("[%tc][%s] Message: %s %n", new Date(), level, message));
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Get IOException: " + e.getMessage());
+        } catch (FileMaxSizeReachedException e) {
+            System.out.println("Get FileMaxSizeReachedException: " + e.getMessage());
+        }
+    }
+    public static void debug(String message) {
+        writeMessage(message, LoginLevel.DEBUG.name());
+    }
+
+    public static void info(String message) {
+        writeMessage(message, LoginLevel.INFO.name());
+    }
 }
